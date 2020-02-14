87bb0740774a7ec0f8c23b24ea068a07b5a48953
Jianxing Ma <59874802+jianxing-ma@users.noreply.github.com>
2020-02-14 08:34:12 -0500

    Cursor movement class
    To be furnished

---
+++ Cursor.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Space_Game
+{
+    class Cursor
+    {
+        public void WriteAt(string s, int x, int y)
+        {
+            try
+            {
+                Console.SetCursorPosition(x, y);
+                Console.Write(s);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.Clear();
+                Console.WriteLine(e.Message);
+            }
+        }
+
+
+        public void WriteLineAt(string s, int range, int x, int y)
+        {
+            try
+            {
+                for (int i = 0; i <range; i++)
+                {
+                    Console.SetCursorPosition(x+i, y);
+                    Console.Write(s);
+                }
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.Clear();
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        public void MoveCharacter()
+        {
+            try
+            {
+                switch (Console.ReadKey(false).Key)
+                {
+                    case ConsoleKey.UpArrow:
+                        Console.SetCursorPosition(Console.CursorLeft - 1, Console.CursorTop - 1);
+                        Console.Write("x");
+                        Console.SetCursorPosition(Console.CursorLeft - 1, Console.CursorTop);
+                        break;
+                    case ConsoleKey.DownArrow:
+                        Console.SetCursorPosition(Console.CursorLeft - 1, Console.CursorTop + 1);
+                        Console.Write("x");
+                        Console.SetCursorPosition(Console.CursorLeft - 1, Console.CursorTop);
+                        break;
+                    case ConsoleKey.LeftArrow:
+                        Console.SetCursorPosition(Console.CursorLeft - 2, Console.CursorTop);
+                        Console.Write("x");
+                        Console.SetCursorPosition(Console.CursorLeft - 1, Console.CursorTop);
+                        break;
+                    case ConsoleKey.RightArrow:
+                        Console.Write("x");
+                        Console.SetCursorPosition(Console.CursorLeft - 1, Console.CursorTop);
+                        break;
+                    case ConsoleKey.X:
+                        Console.Write("x");
+                        Console.SetCursorPosition(Console.CursorLeft - 1, Console.CursorTop);
+                        break;
+
+                }
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.Write("\bx");
+            }
+        }
+
+
+
+    }
+}
