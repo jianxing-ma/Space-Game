d12cd9dbd06af8d1c267151626498d8612f4e85a
Jianxing Ma <59874802+jianxing-ma@users.noreply.github.com>
2020-02-18 12:58:57 -0500

    Added Map Coordinate System
    Refer to EarthMapBoundaries

--- Display.cs
+++ Display.cs
@@ -6,7 +6,7 @@ namespace Space_Game
 {
     class Display
     {
-        private static int Sx1 = 9, Sx2 = 150, Sy1 = 4, Sy2 = 35, Sy3 = 41;
+        public static int Sx1 = 9, Sx2 = 150, Sy1 = 4, Sy2 = 35, Sy3 = 41;
 
         public static void Screen()
         {          
@@ -57,7 +57,42 @@ namespace Space_Game
         }
 
 
+        public static void EarthMap()
+        {
+            for (int i = Sx1 + 5; i < Sx2 - 5; i++)
+                for (int j = Sy1 + 5; j < Sy1 + 10; j++)
+                {
+                    Cursor.WriteAt("#", i, j);
+                }
+        }
 
+        public static List<(int,int)> EarthMapBoundaries()
+        {
+            List<(int, int)> Boundaries = new List<(int, int)>();
+
+            for (int i = Sx1; i < Sx2 + 1; i++)
+            {
+                Boundaries.Add((i, Sy1));
+                Boundaries.Add((i, Sy2));
+            }
+            for (int i = Sy1; i < Sy2 + 1; i++)
+            {
+                Boundaries.Add((Sx1, i));
+                Boundaries.Add((Sx2, i));
+            }
+
+            for (int i = Sx1 + 5; i < Sx2 - 4; i++)
+            {
+                Boundaries.Add((i, Sy1 + 5));
+                Boundaries.Add((i, Sy1 + 9));
+            }
+            for (int j = Sy1 + 5; j < Sy1 + 10; j++)
+            {
+                Boundaries.Add((Sx1 + 6, j));
+                Boundaries.Add((Sx2 -5, j));
+            }
+            return Boundaries;
+        }
 
 
     }
