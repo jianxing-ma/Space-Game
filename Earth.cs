f716919cedec0e88a4bb25801b16a7e9ec034385
luisAsaenz <59874758+luisAsaenz@users.noreply.github.com>
2020-02-20 11:23:21 -0500

    Update Earth. Added boundaries

--- Earth.cs
+++ Earth.cs
@@ -6,72 +6,212 @@ namespace SpaceAdventure
 {
     public class Earth
     {
-
-        public Earth()
+        public static List<(int, int)> eBound = new List<(int, int)>();
+        public static List<(int, int)> moa = new List<(int, int)>();
+        public Earth(int x = 42, int y = 5)
         {
+
             Console.WriteLine("\t\t\t\t\t\tEARTH");
-            Cursor player = new Cursor();
             EarthMap();
+            Console.SetCursorPosition(x, y);
             do
             {
-                player.MoveCharacter();
+                Cursor.MoveCharacter(MapBound());
             } while(true);
         }
 
 
-        private void EarthMap()
+        public static void EarthMap()
         {
 
-                char b = ' ';
-                char s = ' ';
-                char t = 't';
-                char w = 'w';
-                char r = 'r';
+            char b = ' ';
+            char s = ' ';
+            char t = 't';
+            char w = 'w';
+            char r = 'r';
             char H = 'H';
-                Console.BackgroundColor = ConsoleColor.Red;
-                Console.SetCursorPosition(41, 4);
-                for (int i = 0; i <= 100; i++)
-                {
-                    Console.Write(b);
-                }
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.SetCursorPosition(41, 4);
+            for (int i = 0; i <= 100; i++)
+            {
+                Console.Write(b);
+            }
 
-                for (int j = 4; j <= 30; j++)
-                {
+            for (int j = 4; j <= 30; j++)
+            {
 
-                    Console.SetCursorPosition(41, j);
-                    Console.Write(s);
-                }
+                Console.SetCursorPosition(41, j);
+                Console.Write(s);
+            }
 
-                for (int a = 4; a <= 30; a++)
-                {
-                    Console.SetCursorPosition(142, a);
+            for (int a = 4; a <= 30; a++)
+            {
+                Console.SetCursorPosition(142, a);
 
-                    Console.WriteLine(s);
-                }
+                Console.WriteLine(s);
+            }
 
-                Console.SetCursorPosition(42, 30);
-                for (int j = 0; j <= 99; j++)
-                {
+            Console.SetCursorPosition(42, 30);
+            for (int j = 0; j <= 99; j++)
+            {
 
-                    Console.Write(b);
-                }
-                Console.ResetColor();
-                //Stats will go here in a different class
-                Console.WriteLine("\n\ndone");
+                Console.Write(b);
+            }
+            Console.ResetColor();
+            //Stats will go here in a different class
 
-                Trees(t);
+            Console.WriteLine("\n\ndone");
 
-                Water(w);
+            Trees(t);
 
-                Dirt(r);
+            Water(w);
 
-            MissionAndObstacles(H);
+            Rock(r);
 
-               Console.SetCursorPosition(42, 5);
+            MissionAndObstaclesEarth(H);
+
            // Console.Read();
         }
 
-        private void MissionAndObstacles(char H)
+        private List<(int, int)> MOA()
+        {
+            moa.Add((139, 17));
+            moa.Add((47, 24));
+            moa.Add((122, 27));
+            return moa;
+        }
+
+        private List<(int, int)> MapBound()
+        {
+            //Screen Boundaries
+            for (int i = 41; i <= 141; i++)
+            {
+                eBound.Add((i, 4));
+                eBound.Add((i, 30));
+            }
+            for (int i = 4; i <= 30; i++)
+            {
+                eBound.Add((41, i));
+                eBound.Add((142, i));
+            }
+            //Rock Boundaries
+            for (int i = 5; i <= 10; i++)
+            {
+                eBound.Add((103, i));
+            }
+            for (int i = 11; i <= 12; i++)
+            {
+                eBound.Add((115, i));
+            }
+            for (int i = 13; i <= 16; i++)
+            {
+                eBound.Add((123, i));
+            }
+            for (int i = 104; i <= 114; i++)
+            {
+                eBound.Add((i, 10));
+            }
+            for (int i = 116; i <= 122; i++)
+            {
+                eBound.Add((i, 12));
+            }
+            for (int i = 124; i <= 141; i++)
+            {
+                eBound.Add((i, 16));
+            }
+            for (int i = 124; i <= 141; i++)
+            {
+                eBound.Add((i, 18));
+            }
+            for (int i = 19; i <= 29; i++)
+            {
+                eBound.Add((124, i));
+            }
+            //Trees Boundary
+            for (int i = 5; i <= 8; i++)
+            {
+                eBound.Add((44, i));
+                eBound.Add((100, i));
+                if (i == 8)
+                {
+                    for (int j = 45; j <= 100; j++)
+                    {
+                        eBound.Add((j, 8));
+                    }
+                }
+            }
+            for (int i = 16; i <= 29; i++)
+            {
+                if (i >= 16 && i <= 20)
+                {
+                    for (int j = 42; j <= 90; j++)
+                    {
+                        eBound.Add((j, i));
+                    }
+                }
+                else if (i >= 21 && i <= 22)
+                {
+                    for (int j = 42; j <= 45; j++)
+                    {
+                        eBound.Add((j, i));
+                    }
+                }
+                else if (i >= 23 && i <= 25)
+                {
+                    for (int j = 42; j <= 45; j++)
+                    {
+                        eBound.Add((j, i));
+
+                    }
+                    for (int j = 49; j <= 121; j++)
+                    {
+                        eBound.Add((j, i));
+
+                    }
+                }
+                else if (i <= 29)
+                {
+                    for (int j = 42; j <= 120; j++)
+                    {
+                        eBound.Add((j, i));
+                    }
+                }
+            }
+            //Water Boundary
+            for (int i = 42; i <= 80; i++)
+            {
+                eBound.Add((i, 10));
+                eBound.Add((i + 15, 11));
+                eBound.Add((i + 20, 12));
+                eBound.Add((i + 25, 13));
+                eBound.Add((i + 20, 14));
+                eBound.Add((i + 15, 15));
+                eBound.Add((i, 16));
+            }
+            //Mission and Obstacles
+            for (int i = 95; i <= 107; i += 2)
+            {
+                eBound.Add((i, 17));
+            }
+            for (int i = 95; i <= 112; i += 2)
+            {
+                eBound.Add((i, 21));
+            }
+            for(int i = 17; i <= 21; i++)
+            {
+                eBound.Add((95, i));
+                eBound.Add((113, i));
+            }
+            //Space Ship
+            eBound.Add((140, 17));
+            //Treasure
+            eBound.Add((47, 25));
+            eBound.Add((122, 28));
+
+            return eBound;
+        }
+
+        private static void MissionAndObstaclesEarth(char H)
         {
             Console.BackgroundColor = ConsoleColor.Yellow;
             Console.SetCursorPosition(47, 25);
@@ -85,33 +225,63 @@ namespace SpaceAdventure
             {
                 Console.SetCursorPosition(j, 17);
                 Console.Write(H);
+                 if (j == 103)
+                 {
+                 Console.SetCursorPosition(j, 17);
+                 Console.ForegroundColor = ConsoleColor.Red;
+                 Console.Write('S');
+                 Console.ForegroundColor = ConsoleColor.DarkCyan;
+
+                }
             }
             for (int j = 95; j <= 112; j += 2)
             {
                 Console.SetCursorPosition(j, 21);
                 Console.Write(H);
+                if (j == 97)
+                {
+                    Console.SetCursorPosition(j, 21);
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.Write('S');
+                    Console.ForegroundColor = ConsoleColor.DarkCyan;
+                }
             }
             for (int j = 17; j <= 21; j ++)
             {
                 Console.SetCursorPosition(95, j);
                 Console.Write(H);
+                if (j == 18)
+                {
+                    Console.SetCursorPosition(95, j);
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.Write('S');
+                    Console.ForegroundColor = ConsoleColor.DarkCyan;
+                }
             }
             for (int j = 17; j <= 21; j ++)
             {
                 Console.SetCursorPosition(113, j);
                 Console.Write(H);
+                if (j == 20)
+                {
+                    Console.SetCursorPosition(113, j);
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.Write('S');
+                    Console.ForegroundColor = ConsoleColor.DarkCyan;
+                }
             }
             Console.ResetColor();
 
             Console.SetCursorPosition(140, 17);
 
             Console.ForegroundColor = ConsoleColor.DarkMagenta;
-            Console.Write("S");
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            Console.Write((char)0x25B2);
             Console.ResetColor();
 
         }
 
-        private static void Dirt(char r)
+        private static void Rock(char r)
         {
             Console.ForegroundColor = ConsoleColor.DarkGray;
             for (int i = 5; i <= 16; i++)
